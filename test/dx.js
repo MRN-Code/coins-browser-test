c9ac462558ea2c53787c603715963b663d3a0254
dlandis <dlandis@mrn.org>
2016-11-17 16:32:38 -0700

    modernize syntax and linting.  No functional changes

--- test/dx.js
+++ test/dx.js
@@ -1,50 +1,49 @@
+'use strict';
 // Test for Data Exchange
-
-var config = require('config');
-var client = require('./lib/client').client;
-var nav    = require('./lib/nav/navigation')(client, config);
-var micis  = require('./lib/auth/micis')(client);
-var should = require('should');
-
+const config = require('config');
+const client = require('./lib/client').client;
+const nav = require('./lib/nav/navigation')(client, config);
+const micis = require('./lib/auth/micis')(client);
+const should = require('should');
 
 describe('Test data exchange functionality', function() {
-    this.timeout(config.defaultTimeout);
+  this.timeout(config.defaultTimeout);
 
-    before('initialize', function(done) {
-        client.clientReady.then(function boot() {
-            if (!micis.loggedOn) {
-                micis.logon();
-            }
+  before('initialize', function(done) {
+    client.clientReady.then(function boot() {
+      if (!micis.loggedOn) {
+        micis.logon();
+      }
 
-            client.call(done);
+      client.call(done);
+    });
+  });
+
+  it('should be accessible', function(done) {
+    nav
+      .micisMenu
+      .clickNested('Browse Available Data')
+      .call(done);
+  });
+
+  it('should load templates', function(done) {
+    client
+      .elements("#requestMenu option", function(error, response) {
+        response.value.length.should.be.greaterThan(1);
+      })
+      .call(done);
+  });
+
+  it('should load four modalities', function(done) {
+    client
+      .pause(1000)
+      .getText('.statisticsLabel label', function(error, response) {
+        const modalities = [];
+        response.forEach(function(text) { modalities.push(text); });
+        ['MR', 'Assessments', 'Studies', 'Subjects'].forEach(function(modality) {
+          modalities.should.containEql(modality);
         });
-    });
-
-    it('should be accessible', function(done) {
-        nav.micisMenu
-            .clickNested('Browse Available Data')
-            .call(done);
-    });
-
-    it('should load templates', function(done) {
-        client
-            .elements("#requestMenu option", function(error, response) {
-                response.value.length.should.be.greaterThan(1);
-            });
-
-        client.call(done);
-    });
-
-    it('should load four modalities', function(done) {
-        client.pause(1000);
-        client.getText('.statisticsLabel label', function(error, response) {
-            var modalities = [];
-            response.forEach(function(text) { modalities.push(text); });            
-            ['MR', 'Assessments', 'Studies', 'Subjects'].forEach(function(modality) {
-                modalities.should.containEql(modality);
-            });
-        });
-
-        client.call(done);
-    });
+      })
+      .call(done);
+  });
 });
