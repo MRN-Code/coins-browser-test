a2d1d6229b51c5158eb26354293f13d49c563a2e
dlandis <dlandis@mrn.org>
2016-11-28 13:30:20 -0700

    clean up new request test and remove a bunch of comments

--- test/dx-new-request.js
+++ test/dx-new-request.js
@@ -5,7 +5,7 @@ const nav = require('./lib/nav/navigation')(client, config);
 const micis = require('./lib/auth/micis')(client);
 const should = require('should');
 
-describe('Test data exchange functionality', function() {
+describe('Test data exchange functionality for a new request', function() {
   this.timeout(config.defaultTimeout);
 
   before('initialize', function(done) {
@@ -26,8 +26,10 @@ describe('Test data exchange functionality', function() {
   });
 
   it('should verify that [Previous Request Templates] have loaded', function(done) {
+    const previousRequestTemplates = '#requestMenu > option';
+
     client
-      .elements("#requestMenu > option")
+      .elements(previousRequestTemplates)
       .then(response => {
         response.value.length.should.be.greaterThan(1);
       })
@@ -35,27 +37,34 @@ describe('Test data exchange functionality', function() {
   });
 
   it('should verify that all 4 modality filters have loaded', function(done) {
-    const modalityFilters = ['MR', 'Assessments', 'Studies', 'Subjects'];
+    const mrFilter = 'div.draggable.filter.MRFilter.ui-draggable';
+    const filterContainer = 'div#filterContainer > div.statisticsLabel > div > label';
 
     client
-      .waitForExist('div.draggable.filter.MRFilter.ui-draggable', 5000)
-      .getText('div#filterContainer > div.statisticsLabel > div > label')
+      .waitForExist(mrFilter)
+      .getText(filterContainer)
       .then(response => {
-        response.should.be.eql(modalityFilters);
+        response.should.be.eql(['MR', 'Assessments', 'Studies', 'Subjects']);
       })
       .call(done);
   });
 
   it('should click on [Untitled Request] and add a new title', function(done) {
-    const title = 'New Request Title Test';
+    const titleElement = 'div#requestTitle';
+    const titleInput = 'input#newRequestTitle';
+    const titleValue = 'New Request Title Test';
+    const okButton = 'input#okButton';
 
     client
-      .click('div#requestTitle')
-      .setValue('input#newRequestTitle', title)
-      .click('input#okButton')
-      .getText('div#requestTitle')
+      .waitForExist(titleElement)
+      .click(titleElement)
+      .setValue(titleInput, titleValue)
+      .waitForExist(okButton)
+      .click(okButton)
+      .pause(500)
+      .getText(titleElement)
       .then(returnedTitle => {
-        returnedTitle.should.be.equal(title)
+        returnedTitle.should.be.equal(titleValue)
       })
       .call(done);
   });
@@ -67,25 +76,19 @@ describe('Test data exchange functionality', function() {
     const requestWorkspace = 'div.group.andGroup.ui-droppable > a#groupSwitch';
     const label = 'ul.attributeList > li:nth-child(3)';
     const select = `${label} > div.attributeOptionContainer > select`;
+    const applyButton = 'input#applyButton';
+    const statistics = 'div#filterContainer > div.statisticsLabel > div.statisticsValue';
 
     client
-      // Drag MR filter into the request workspace
       .dragAndDrop(mrFilter, requestWorkspace)
-      // Wait for the [Label] element to exist, then click on it
       .waitForExist(label)
       .click(label)
-      // Select the [ANAT] option
       .waitForExist(select)
       .selectByValue(select, 'ANAT')
-      // Click [Apply] button
-      .waitForExist('input#applyButton')
-      .click('input#applyButton')
-      // Pause until statistics update
-      // Note: webdriverio 2.4.5 does not have waitUntil.  So this is a hack
-      // that should eventually be removed :(
+      .waitForExist(applyButton)
+      .click(applyButton)
       .pause(2000)
-      // Verify Statistics
-      .getText('div#filterContainer > div.statisticsLabel > div.statisticsValue')
+      .getText(statistics)
       .then(response => {
         response.should.be.eql([ '384', '0', '1', '203' ]);
       })
@@ -93,16 +96,21 @@ describe('Test data exchange functionality', function() {
   });
 
   it('should update the request title again', function(done) {
-    const title = 'Second request title update';
+    const titleElement = 'div#requestTitle';
+    const titleInput = 'input#newRequestTitle';
+    const titleValue = 'Second request title update';
+    const okButton = 'input#okButton';
 
     client
-      .click('div#requestTitle')
-      .setValue('input#newRequestTitle', title)
-      .click('input#okButton')
-      .pause(1000)
-      .getText('div#requestTitle')
+      .waitForExist(titleElement)
+      .click(titleElement)
+      .setValue(titleInput, titleValue)
+      .waitForExist(okButton)
+      .click(okButton)
+      .pause(500)
+      .getText(titleElement)
       .then(returnedTitle => {
-        returnedTitle.should.be.equal(title)
+        returnedTitle.should.be.equal(titleValue)
       })
       .call(done);
   });
@@ -112,25 +120,19 @@ describe('Test data exchange functionality', function() {
     const requestWorkspace = 'div#request > div > div.filter.MRFilter';
     const studyName = 'div#ui-id-21 > ul.attributeList > li.attributeItem';
     const select = `${studyName} > div > select`;
+    const applyButton = 'div#ui-id-23 > input#applyButton';
+    const statistics = 'div#filterContainer > div.statisticsLabel > div.statisticsValue';
 
     client
-      // Drag Study filter into the request workspace
       .dragAndDrop(studyFilter, requestWorkspace)
-      // Wait for the [Study Name] element to exist, then click on it
       .waitForExist(studyName)
       .click(studyName)
-      // Select the [ABIDE] option
       .waitForExist(select)
       .selectByVisibleText(select, 'ABIDE (1)')
-      // Click [Apply] button
-      .waitForExist('div#ui-id-23 > input#applyButton')
-      .click('div#ui-id-23 > input#applyButton')
-      // Pause until statistics update
-      // Note: webdriverio 2.4.5 does not have waitUntil.  So this is a hack
-      // that should eventually be removed :(
-      .pause(3000)
-      // Verify Statistics
-      .getText('div#filterContainer > div.statisticsLabel div.statisticsValue')
+      .waitForExist(applyButton)
+      .click(applyButton)
+      .pause(2000)
+      .getText(statistics)
       .then(response => {
         response.should.be.eql([ '0', '0', '0', '0' ]);
       })
@@ -139,15 +141,13 @@ describe('Test data exchange functionality', function() {
 
   it('should change the request workspace group to an [OR] group', function(done) {
     const switchButton = 'div#request > div a#groupSwitch';
+    const statistics = 'div#filterContainer > div.statisticsLabel > div.statisticsValue';
 
     client
+      .waitForExist(switchButton)
       .click(switchButton)
-      // Pause until statistics update
-      // Note: webdriverio 2.4.5 does not have waitUntil.  So this is a hack
-      // that should eventually be removed :(
-      .pause(3000)
-      // Verify Statistics
-      .getText('div#filterContainer > div.statisticsLabel > div.statisticsValue')
+      .pause(2000)
+      .getText(statistics)
       .then(response => {
         response.should.be.eql([ '2637', '1112', '2', '1315' ]);
       })
@@ -161,26 +161,21 @@ describe('Test data exchange functionality', function() {
     const andGroupPostMove = 'div#request > div > div.group.andGroup.ui-droppable';
     const subjectType = 'div#ui-id-37 > ul.attributeList > li:nth-child(1)';
     const select = `${subjectType} > div > select`;
+    const applyButton = 'div#ui-id-39 > input#applyButton';
+    const statistics = 'div#filterContainer > div.statisticsLabel > div.statisticsValue';
 
     client
       .dragAndDrop(andGroupPreMove, requestWorkspace)
       .pause(1000)
       .dragAndDrop(subjectsFilter, andGroupPostMove)
-      // Wait for the [Subject Type] element to exist, then click on it
       .waitForExist(subjectType)
       .click(subjectType)
-      // Select the [Subject Type] option
       .waitForExist(select)
       .selectByVisibleText(select, '20-29 (72)')
-      // Click [Apply] button
-      .waitForExist('div#ui-id-39 > input#applyButton')
-      .click('div#ui-id-39 > input#applyButton')
-      // Pause until statistics update
-      // Note: webdriverio 2.4.5 does not have waitUntil.  So this is a hack
-      // that should eventually be removed :(
-      .pause(3000)
-      // Verify Statistics
-      .getText('div#filterContainer > div.statisticsLabel > div.statisticsValue')
+      .waitForExist(applyButton)
+      .click(applyButton)
+      .pause(2000)
+      .getText(statistics)
       .then(response => {
         response.should.be.eql([ '3748', '2396', '3', '1386' ]);
       })
@@ -193,10 +188,9 @@ describe('Test data exchange functionality', function() {
     const requestPopUp = '#ui-id-45 > ul.requestSourceList > h3';
 
     client
+      .waitForExist(sendRequest)
       .click(sendRequest)
-      // WHAT!?  waitForExist does not wait long enough :(
-      // I needed to add this pause, or else waitForExist fails...
-      .pause(4000)
+      .pause(3000)
       .waitForExist(requestPopUp)
       .getText(requestPopUp)
       .then(response => {
