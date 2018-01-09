b522a01df0fdd367d321a3f3c64610e3f15b8e41
Jonathan Lake <jwtlake@gmail.com>
2018-01-09 13:23:05 -0800

    update requestMenu tests for new subgroup sections

--- test/dx-clone.js
+++ test/dx-clone.js
@@ -22,10 +22,10 @@ describe('Test data exchange functionality for cloning a request', function dxCl
     });
   });
 
-  it('should verify that [Previous Request Templates] have loaded', (done) => {
+  it('should verify that [Open Previous Request Templates] have loaded', (done) => {
     client
       .pause(500)
-      .elements('#requestMenu > option')
+      .elements('#requestMenu option')
       .then((response) => {
         response.value.length.should.be.greaterThan(1);
       })
@@ -42,12 +42,12 @@ describe('Test data exchange functionality for cloning a request', function dxCl
       .call(done);
   });
 
-  it('should select a previous request template', (done) => {
-    const select = 'select#requestMenu';
+  it('should load a Public Templates', (done) => {
+    const publicTemplates = 'select#requestMenu > optgroup[label="Public Templates"]';
 
     client
-      .waitForExist(select)
-      .selectByValue(select, 460)
+      .waitForExist(publicTemplates)
+      .selectByValue(publicTemplates, 460)
       .click('input#requestOpenButton')
       .pause(3000)
       .click('div#requestStatusMessage > a.symlink')
