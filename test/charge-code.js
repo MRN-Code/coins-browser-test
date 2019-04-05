36c8f437504a68b606e4f4096d17653fadbafe5a
Srikanth Muttavarapu <suttavarapu@mrn.org>
2019-04-05 13:27:54 -0600

    rename test and add study to charge code

--- test/charge-code.js
+++ test/charge-code.js
@@ -15,7 +15,7 @@
 const nav = require('./lib/nav/navigation')(browser);
 const micis = require('./lib/auth/micis')(browser);
 
-const sampleData = browser.options.testData.chargeCodeAdd.sampleData;
+const sampleData = browser.options.testData.chargeCode.sampleData;
 
 describe('Add a new Charge Code', () => {
   before('initialize', () => {
@@ -41,16 +41,17 @@ describe('Add a new Charge Code', () => {
      .setValue('input[name=charge_code]', sampleData.chargeCode)
      .setValue('input[name=project_number]', sampleData.projectNumber)
      .setValue('input[name=start_date]', sampleData.startDate)
-     .setValue('input[name=end_date]', sampleData.endDate);
+     .click('#page-container > div.boxHeader > span')  // to close the calender
+     .setValue('input[name=end_date]', sampleData.endDate)
+     .click('#page-container > div.boxHeader > span');
   });
   it('should submit and see if successful', () => {
-    browser.pause(3000);
     browser.click('input[type=button][value=\'Add Charge Code\']').waitForPaginationComplete();
-    browser.pause(3000);
     browser.getText('#page-container > div:nth-child(3) > div > center > font').should.be.equal('Charge Code Successfully Added!');
   });
   it('should verify the data', () => {
-    browser.click('input[type=button][value=\'View All Charge Codes\']').waitForPaginationComplete();
+    browser.click('input[type=button][value=\'View All Charge Codes\']').pause(500);
+    browser.waitForExist('input[type=search]');
     browser.setValue('input[type=search]', sampleData.chargeCode);
     browser.click('#charge_codes > tbody > tr > td:nth-child(3) > a').waitForPaginationComplete();
 
@@ -63,5 +64,18 @@ describe('Add a new Charge Code', () => {
     } else {
       browser.isSelected('input[name=scan_limited][value=f]').should.be.true();
     }
+  });
+  it('should add study to the charge code', () => {
+    browser.click('input[type=button][value=\'Add Study\']').waitForPaginationComplete();
+    browser
+        .selectByVisibleText('#funding_source', sampleData.fundingSource)
+        .element('#study_id')
+        .selectByAttribute('value', sampleData.studyID);
+    browser
+        .click('input[name=is_enabled][value=t]')
+        .click('#modality_label_1')
+        .click('input[type=button][value=\'Add Charge Code Study\']')
+        .pause(500);
+    browser.getText('#page-container > div.boxBody > div > center > font').should.containEql('Charge Code Study Successfully Added!');
   });
 });
